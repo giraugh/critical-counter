fe2bea3fa3c605c9c3cc315f02da143e05309e18
retroverse <ebreakey@gmail.com>
2021-01-12 14:30:07 +1100

    Add simple bot setup

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    ( runBot
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Discord as D
+import qualified Discord.Types as D
+import qualified Discord.Requests as D
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import           System.IO (hPutStrLn, stderr)
+import           System.Environment (lookupEnv)
+import           Control.Monad (when, void)
+
+runBot :: IO ()
+runBot = do
+    tokenM <- lookupEnv "DISCORD_TOKEN"
+    case tokenM of
+        Nothing ->
+            hPutStrLn stderr "Expected environment variable DISCORD_TOKEN"
+        Just token -> do
+            putStrLn "Starting bot..."
+            userFacingError <- D.runDiscord $ D.def
+                { D.discordToken = T.pack token
+                , D.discordOnEvent = eventHandler
+                }
+            TIO.putStrLn userFacingError
+
+eventHandler :: D.Event -> D.DiscordHandler ()
+eventHandler event = case event of
+    D.MessageCreate m ->
+        when (not (fromBot m) && isPing (D.messageText m)) $ do
+            _ <- D.restCall (D.CreateReaction (D.messageChannel m, D.messageId m) "eyes")
+            _ <- D.restCall (D.CreateMessage (D.messageChannel m) "Pong!")
+            pure ()
+    _ -> pure ()
+
+fromBot :: D.Message -> Bool
+fromBot = D.userIsBot . D.messageAuthor
+
+isPing :: T.Text -> Bool
+isPing = ("!ping" `T.isPrefixOf`) . T.toLower
