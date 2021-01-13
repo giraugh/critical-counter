6c91944d63983937f8ad90d4229d9a0f941cd50b
retroverse <ebreakey@gmail.com>
2021-01-13 22:07:16 +1100

    Add crit add commands

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,12 +9,23 @@ import qualified Discord.Types as D
 import qualified Discord.Requests as D
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import qualified Data.Map as Map
+import qualified Control.Monad.Reader as Reader
+import           Data.IORef (newIORef, readIORef, writeIORef, modifyIORef, IORef)
 import           System.IO (hPutStrLn, stderr)
 import           System.Environment (lookupEnv)
 import           Control.Monad (when, void)
+import           Control.Monad.Trans (liftIO)
+
+data CritType = CritSuccess | CritFailure deriving (Eq)
+data BotState = BotState {
+      successes :: Map.Map D.UserId Int
+    , failures  :: Map.Map D.UserId Int
+    }
 
 runBot :: IO ()
 runBot = do
+    stateRef <- newIORef $ BotState {successes = Map.empty, failures = Map.empty}
     tokenM <- lookupEnv "DISCORD_TOKEN"
     case tokenM of
         Nothing ->
@@ -23,21 +34,78 @@ runBot = do
             putStrLn "Starting bot..."
             userFacingError <- D.runDiscord $ D.def
                 { D.discordToken = T.pack token
-                , D.discordOnEvent = eventHandler
+                , D.discordOnEvent = eventHandler stateRef
                 }
             TIO.putStrLn userFacingError
 
-eventHandler :: D.Event -> D.DiscordHandler ()
-eventHandler event = case event of
-    D.MessageCreate m ->
-        when (not (fromBot m) && isPing (D.messageText m)) $ do
-            _ <- D.restCall (D.CreateReaction (D.messageChannel m, D.messageId m) "eyes")
-            _ <- D.restCall (D.CreateMessage (D.messageChannel m) "Pong!")
-            pure ()
-    _ -> pure ()
+eventHandler :: IORef BotState -> D.Event -> D.DiscordHandler ()
+eventHandler stateRef event =
+    case event of
+        D.MessageCreate m -> when (not (fromBot m)) $ handleCommand stateRef m
+        _                 -> pure ()
+
+handleCommand :: IORef BotState -> D.Message -> D.DiscordHandler ()
+handleCommand stateRef m
+    | isCommandWithMention "!crit20" m = handleAddCommand stateRef CritSuccess m
+    | isCommandWithMention "!crit1" m = handleAddCommand stateRef CritFailure m
+    | otherwise = pure ()
+
+handleAddCommand :: IORef BotState -> CritType -> D.Message -> D.DiscordHandler ()
+handleAddCommand stateRef critType m = Reader.ask >>= \handle -> liftIO $ do
+    -- React to the message
+    Reader.runReaderT (createReaction reactionName m) handle
+
+    -- Add a crit to the state
+    case critType of
+        CritSuccess -> modifyIORef stateRef $ \state -> BotState {
+              failures = failures state
+            , successes = Map.insertWith (+) userId 1 (successes state)
+            }
+        CritFailure -> modifyIORef stateRef $ \state -> BotState {
+              successes = successes state
+            , failures = Map.insertWith (+) userId 1 (failures state)
+            }
+
+    -- Log the username (Debug Purposes)
+    putStrLn $ "Added a crit for user: " ++ (show $ userId)
+
+    -- Read the new total
+    state <- readIORef stateRef
+    let accessor = if critType == CritSuccess then successes else failures
+        map      = accessor state
+        countM   = Map.lookup userId map
+
+    -- Log the count (Debug Purposes)
+    putStrLn $ show countM
+
+    -- Report current count
+    let count       = maybe 0 id countM 
+        critTypeStr = if critType == CritSuccess then "successes" else "failures"
+        uname = T.unpack $ D.userName targetUser
+        msg         = uname ++ " has " ++ (show count) ++ " crit " ++ critTypeStr
+    Reader.runReaderT (createMessage (T.pack msg) m) handle
+
+    pure ()
+    where
+        reactionName = if critType == CritSuccess then "ok_hand" else "cry"
+        targetUser   = head $ D.messageMentions m
+        userId       = D.userId $ targetUser 
+
+createReaction :: T.Text -> D.Message -> D.DiscordHandler (Either D.RestCallErrorCode ())
+createReaction reactionName m =
+    D.restCall (D.CreateReaction (D.messageChannel m, D.messageId m) reactionName)
+
+createMessage :: T.Text -> D.Message -> D.DiscordHandler (Either D.RestCallErrorCode D.Message)
+createMessage messageText m =
+    D.restCall (D.CreateMessage (D.messageChannel m) messageText)
 
 fromBot :: D.Message -> Bool
 fromBot = D.userIsBot . D.messageAuthor
 
-isPing :: T.Text -> Bool
-isPing = ("!ping" `T.isPrefixOf`) . T.toLower
+isCommandWithMention :: T.Text -> D.Message -> Bool
+isCommandWithMention prefix m = 
+    let hasPrefix = (prefix `T.isPrefixOf`) $ T.toLower $ D.messageText m
+        hasMention = 1 == (length $ D.messageMentions m)
+    in hasPrefix && hasMention
+
+
