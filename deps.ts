bcd621f0a99796113de69228d3dc906ce12077c0
[Ewan Breakey s3845382] <s3845382@student.rmit.edu.au>
2021-05-18 13:40:05 +1000

    Specify dep versions

--- deps.ts
+++ deps.ts
@@ -3,8 +3,8 @@ export {
     cache,
     fetchMembers,
     sendMessage
-} from 'https://deno.land/x/discordeno/mod.ts'
-export type { Message, Embed, Member, Guild } from 'https://deno.land/x/discordeno/mod.ts'
+} from 'https://deno.land/x/discordeno@10.5.0/mod.ts'
+export type { Message, Embed, Member, Guild } from 'https://deno.land/x/discordeno@10.5.0/mod.ts'
 export { serve } from 'https://deno.land/std@0.87.0/http/server.ts'
 export { config } from 'https://deno.land/x/dotenv/mod.ts'
 export { Client } from "https://deno.land/x/postgres/mod.ts"
